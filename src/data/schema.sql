6ea50a6ebb2ef517a7fecc1c32c828a060812e1c
Steve Gricci <steve@gricci.org>
2019-05-09 21:35:41 -0600

    Adding just schema SQL file

--- src/data/schema.sql
+++ src/data/schema.sql
@@ -5,25 +5,6 @@ CREATE TABLE `borough_lookup` (
     PRIMARY KEY (`borough_id`)
 );
 
-DROP TABLE IF EXISTS `trip_data`;
-CREATE TABLE `trip_data` (
-  `vendor_id` varchar(10) NOT NULL,
-  `vendor_type` enum('yellow','fhv','green') NOT NULL,
-  `pickup_time` datetime NOT NULL,
-  `dropoff_time` datetime NOT NULL,
-  `pickup_borough` int(10) NOT NULL,
-  `dropoff_borough` int(10) NOT NULL,
-  `trip_distance` decimal(10,2) DEFAULT NULL,
-  `total_amount` decimal(10,2) DEFAULT NULL,
-  `tip_amount` decimal(10,2) DEFAULT NULL,
-  KEY `idx_trip_data_pickup_borough_dropoff_borough` (`pickup_borough`,`dropoff_borough`),
-  KEY `idx_trip_data_pickup_time_dropoff_time` (`pickup_time`,`dropoff_time`),
-  KEY `idx_time_and_boroughs` (`pickup_time`,`dropoff_time`,`pickup_borough`,`dropoff_borough`),
-  KEY `idx_trip_data_vendor_type_pickup_time_dropoff_time` (`vendor_type`,`pickup_time`,`dropoff_time`),
-  KEY `idx_trip_data_vendor_type_pickup_borough_dropoff_borough` (`vendor_type`,`pickup_borough`,`dropoff_borough`),
-  KEY `idx_type_time_borough` (`vendor_type`,`pickup_time`,`dropoff_time`,`pickup_borough`,`dropoff_borough`)
-);
-
 DROP TABLE IF EXISTS `trip_summary`;
 CREATE TABLE `trip_summary` (
   `vendor_type` enum('yellow','fhv','green') NOT NULL,
